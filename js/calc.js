ddde10045a27df1044a9dc6f020cb842036020f3
Scarlett Patton <scarwars05@gmail.com>
2018-06-26 13:35:59 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,18 @@
 //add your code here
+
+let num1 = $('#number1');
+let operator = $('#operation');
+let num2 = $('#number2');
+
+function validate(operator, num1, num2) {
+  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') { 
+    if (isNaN(num1) || num1 === '' || (isNaN(num2)) || num2 === '') {
+      ('#result').text('Sorry, one of those is not a valid number!');
+    } else {
+      answer(operator, num1, num2);
+    }
+  } else {
+    $('#result').html('Sorry, one of those is not a valid number!');
+  }
+}
+
