a60d6763987f64d02c54599214269117908d6607
Scarlett Patton <scarwars05@gmail.com>
2018-06-26 13:37:06 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,18 +1,52 @@
-//add your code here
+// set some variables so we can shortcut them for later use
+let num1 = $('#number1').val();  // grabs number 1
+let num2 = $('#number2').val();  // grabs number 2
+let operator = $('#operation').val();  // grabs the operator
 
-let num1 = $('#number1');
-let operator = $('#operation');
-let num2 = $('#number2');
+//when the equals is clicked, set new variables and then send to the validate function
+$('#equals').on('click', function() {
+    num1 = $('#number1').val();
+    num2 = $('#number2').val();
+    operator = $('#operation').val();
+    validate();
+});
 
-function validate(operator, num1, num2) {
-  if (operator == '+' || operator == '-' || operator == '*' || operator == '/') { 
-    if (isNaN(num1) || num1 === '' || (isNaN(num2)) || num2 === '') {
-      ('#result').text('Sorry, one of those is not a valid number!');
+// validate function lives here
+// number need to be numbers
+// operation should be + - *
+// check for user input, specifically numbers are not empty
+// operator is not empty
+function validate() {
+    if (num1 === '' || num2 === '') {
+        $('#result').html('Input BOTH numbers.');
+    } else if (isNaN(num1) || isNaN(num2)) {
+        $('#result').html('At least one of those is not a number.');
+    } else if (operator === '') {
+        $('#result').html('Your operator is empty. FIX IT!');
+    } else if (operator === '+' || operator === '-' || operator === '*' || operator === '/') {
+        doMath();
     } else {
-      answer(operator, num1, num2);
+        $('#result').html("Ya goof'd on the operator");
     }
-  } else {
-    $('#result').html('Sorry, one of those is not a valid number!');
-  }
 }
 
+//assuming the validation is true, run the calculator
+
+function doMath() {
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    switch (operator) {
+        case '+': 
+        $('#result').html((num1 + num2).toFixed(2));
+        break;
+        case '-':
+        $('#result').html((num1 - num2).toFixed(2));
+        break;
+        case '*':
+        $('#result').html((num1 * num2).toFixed(2));
+        break;
+        case '/':
+        $('#result').html((num1 / num2).toFixed(2));
+        break;
+    }
+}
